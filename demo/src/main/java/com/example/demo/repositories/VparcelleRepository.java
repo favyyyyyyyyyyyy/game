cc262325481377f3245cf6235bb9cc4c6c10dacf
christina <120638626+christinaRas@users.noreply.github.com>
2024-01-19 15:04:02 +0300

    update vparcelle

--- demo/src/main/java/com/example/demo/repositories/VparcelleRepository.java
+++ demo/src/main/java/com/example/demo/repositories/VparcelleRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.domaine.Vparcelle;
 
-public interface VparcelleRepository extends JpaRepository<Vparcelle, Integer> {
 
+public interface VparcelleRepository extends JpaRepository<Vparcelle, Integer> {
     List<Vparcelle> findByNom_culture(String nom_culture);
     // You can add custom query methods here if needed
+
 }
