f172a15e023ba5a23fbd6da65b061f4960b2654a
christina <120638626+christinaRas@users.noreply.github.com>
2024-01-29 09:35:22 +0300

    Update CultureController.java

--- demo/src/main/java/com/example/demo/controllers/CultureController.java
+++ demo/src/main/java/com/example/demo/controllers/CultureController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,17 +16,14 @@ import java.util.List;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/agrigame")
 
 public class CultureController {
     @Autowired
     private CultureService cultureService;
 
-    @GetMapping("/cultures")
-    public List<Culture> getAllCultures() {
-        System.out.println("culture");
-        return cultureService.getAllCultures();
-    }
+
 
     @PostMapping("/culture")
     public Culture insertCulture(@RequestBody Culture culture) {
